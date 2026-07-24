b2ec6d152a72a6f68bb9c50ace57448f7b0f5400
VTU27511 <vtu27511@veltech.edu.in>
2026-07-24 20:53:02 +0530

    first commit

---
+++ task21_07/Salaries.java
@@ -0,0 +1,20 @@
+package ProblemSolvingUsingJava.task21_07;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Salaries {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        List<Integer> li = new ArrayList<>(n);
+        for(int i=0;i<n;i++){
+            li.add(sc.nextInt());
+        }
+        for(int ele:li){
+            System.out.print((int)(ele+(ele*0.1))+" ");
+        }
+
+    }
+}
